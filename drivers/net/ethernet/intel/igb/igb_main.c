70b792689e7263acba60502cfbd5874e0119bc1c
Alan Liebthal <alanl@cumulusnetworks.com>
2015-04-16 08:55:20 -0700

    igb: support SIOCSMIIREG
    Support setting the MII register via SIOCSMIIREG.
    
    Signed-off-by: Alan Liebthal <alanl@cumulusnetworks.com>
    Signed-off-by: Jonathan Toppins <jtoppins@cumulusnetworks.com>

--- drivers/net/ethernet/intel/igb/igb_main.c
+++ drivers/net/ethernet/intel/igb/igb_main.c
@@ -7141,6 +7141,11 @@ static int igb_mii_ioctl(struct net_device *netdev, struct ifreq *ifr, int cmd)
 			return -EIO;
 		break;
 	case SIOCSMIIREG:
+		adapter->hw.phy.addr = data->phy_id;
+		if (igb_write_phy_reg(&adapter->hw, data->reg_num & 0x1F,
+				      data->val_in))
+			return -EIO;
+		break;
 	default:
 		return -EOPNOTSUPP;
 	}
